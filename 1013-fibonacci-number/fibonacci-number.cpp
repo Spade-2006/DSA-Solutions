16c5ed3377f9a07a05f5d8bd06becf8463732d7f
Satyansh Acharya <satyanshacharya8894958062@gmail.com>
2026-09-25 23:36:04 +0530

    Time: 3 ms (64.83%) | Memory: 8.1 MB (16.97%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -17,10 +17,15 @@ public:
     {
         // step1 
         vector<int> dp(n+1,-1);
-        solve(n,dp);
+        
+        dp[0] = 0;
+        if(n>0)
+            dp[1] = 1;
 
-        if(n==0 or n==1)
-            return n;
+        for(int i = 2 ; i<=n ; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
 
         return dp[n];
                
